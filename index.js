69d916ab72772a95c78fb59e8e5ee0460c294ce5
Даниил <Vinland0_0@mail.ru>
2023-04-12 20:03:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import path from 'path';
+import express from 'express';
+const app = express();
+import  http from 'http';
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+import {Server} from'socket.io';
+const io = new Server(server);
+console.clear();
+const __dirname = path.resolve();
+
+app.use(express.static(path.resolve(__dirname,'static')))
+
+io.on('connection',(socket)=>{
+   socket.on('chat message',function(data){
+      io.emit('chat message',{name:data.name, body:data.body});
+   })
+})
+
+
+server.listen(PORT)
---
+++ static/scripts/client.js
@@ -0,0 +1,42 @@
+const formChat = document.getElementById('formChat');
+const input = document.getElementById('input');
+const message = document.getElementById('messages');
+const socket = io();
+
+const formUserName = document.getElementById('formUserName');
+const userName = document.getElementById('userName');
+
+formUserName.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (userName.value) {
+        formUserName.style.display = 'none';
+        formChat.style.display = 'flex';
+    }
+})
+
+formChat.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (input.value) {
+        socket.emit('chat message', { name: userName.value, body: input.value });
+        input.value = '';
+    }
+})
+
+socket.on('chat message', function (data) {
+    const itemHours = new Date().getHours();
+    const itemMinutes = new Date().getMinutes();
+
+    message.insertAdjacentHTML(
+        'afterbegin',
+        `<div class='mess'>
+        <div class='sizeNameAndDate'>${data.name} <label>${itemHours}:${itemMinutes}</label>
+        </div>
+        <li>${data.body}</li>
+        </div>`
+    )
+    window.scrollTo(0, document.body.scrollHeight);
+})
+
+
